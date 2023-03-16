e13c2e943b8c38cbd8e3e627f6da201e14357292
Christophe Hartmann <chartmann.35@gmail.com>
2023-03-16 12:00:15 +0100

    feat(deploy): add purgecss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
   build: {
     transpile: ['vuetify'],
   },
-  modules: ['@pinia/nuxt', '@nuxtjs/i18n', '@nuxt/content', '@vueuse/nuxt'],
+  modules: ['@pinia/nuxt', '@nuxtjs/i18n', '@nuxt/content', '@vueuse/nuxt', 'nuxt-purgecss'],
   i18n: {
     langDir: 'locales',
     lazy: true,
