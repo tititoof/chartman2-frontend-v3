892ca177a1fb1cd3f1328bb48ea09c7ab75e4cdd
Christophe Hartmann <chartmann.35@gmail.com>
2023-09-11 08:06:21 +0200

    fix(tests): add vuetify in config

--- vitest.ci-cd.config.mjs
+++ vitest.ci-cd.config.mjs
@@ -1,17 +1,23 @@
 import { defineVitestConfig } from 'nuxt-vitest/config'
+import vuetify from 'vite-plugin-vuetify'
 
 export default defineVitestConfig({
+  plugins: [
+    vuetify(),
+  ],
   test: {
     environment: 'nuxt',
-    reporters: ['vitest-sonar-reporter'],
-    outputFile: 'sonar-report.xml',
+    deps: { inline: ['vuetify'] },
     globals: true,
     include: ['tests/**/*.nuxt.spec.ts'],
+    exclude: ['api', '.nuxt', 'server', 'middleware', 'layouts', 'pages', 'tests', '*.config.ts', '*.d.ts', 'app.vue'],
+    reporters: ['vitest-sonar-reporter'],
+    outputFile: 'sonar-report.xml',
     setupFiles: ['tests/setup/init.ts'],
     coverage: {
       reporter: 'lcov',
       provider: 'v8',
-      include: ['layouts', 'components', 'store', 'pages'],
+      include: ['layouts', 'components', 'store', 'pages', 'app'],
       // all: true
     }
   },
